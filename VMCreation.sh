6073bed7dc1cbb4ead029f46e0a9e1b49002ee11
Saif Eddine DHIFI <53258132+D-saif@users.noreply.github.com>
2021-11-28 22:30:15 +0100

    Update VMCreation.sh

--- VMCreation.sh
+++ VMCreation.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #Test the json file
-#echo `jq '.[0].vmid' jsonTestFile.json`
-#echo `jq '.[1].vmid' jsonTestFile.json`
+#echo `jq '.[0].vmid' CloudInit_Options.json`
+#echo `jq '.[1].vmid' CloudInit_Options.json`
 
 
-command='jq length jsonTestFile.json'
+command='jq length CloudInit_Options.json'
 size=($($command))
 
 
@@ -20,54 +20,55 @@ arr2=()
  
 for (( i=0; i<$size; i++ ))
 do
-      	echo "qm options for vm${i}:"
+	echo "qm options for vm${i}:"
         echo
-	      command='jq -r '.[${i}].tmpid' jsonTestFile.json'
-	      tmpid=($($command))
-	      echo "  tmpid:$tmpid"
+
+	command='jq -r '.[${i}].tmpid' CloudInit_Options.json'
+	tmpid=($($command))
+	echo "  tmpid:$tmpid"
         
 	
-        command='jq -r '.[${i}].vmid' jsonTestFile.json'
+	command='jq -r '.[${i}].vmid' CloudInit_Options.json'
         vmid=($($command))
         echo "  vmid:$vmid"
 
-        command='jq -r '.[${i}].name' jsonTestFile.json'
+	command='jq -r '.[${i}].name' CloudInit_Options.json'
         name=($($command))
         echo "  name:$name"
 
-      	command='jq -r '.[${i}].ciuser' jsonTestFile.json'
+	command='jq -r '.[${i}].ciuser' CloudInit_Options.json'
         ciuser=($($command))
         echo "  ciuser:$ciuser"
 
-	      command='jq -r '.[${i}].cipassword' jsonTestFile.json'
+	command='jq -r '.[${i}].cipassword' CloudInit_Options.json'
         cipassword=($($command))
         echo "  cipassword:$cipassword"
   
-	      command='jq -r '.[${i}].nameserver' jsonTestFile.json'
+	command='jq -r '.[${i}].nameserver' CloudInit_Options.json'
         nameserver=($($command))
         echo "  nameserver:$nameserver"
 
-        command='jq -r '.[${i}].gw' jsonTestFile.json'
+        command='jq -r '.[${i}].gw' CloudInit_Options.json'
         gw=($($command))
         echo "  gateway:$gw"
 
-        command='jq -r '.[${i}].ip' jsonTestFile.json'
+        command='jq -r '.[${i}].ip' CloudInit_Options.json'
         ip=($($command))
         echo "  ip:$ip"
 
-        command='jq -r '.[${i}].sshkey' jsonTestFile.json'
+        command='jq -r '.[${i}].sshkey' CloudInit_Options.json'
         sshkey=($($command))
         echo "  sshkey:$sshkey"
 
-        command='jq -r '.[${i}].model' jsonTestFile.json'
+        command='jq -r '.[${i}].model' CloudInit_Options.json'
         model=($($command))
         echo "  model:$model"
 	
-        command='jq -r '.[${i}].bridge' jsonTestFile.json'
+        command='jq -r '.[${i}].bridge' CloudInit_Options.json'
         bridge=($($command))
         echo "  bridge:$bridge"
 
-        command='jq -r '.[${i}].keyboard' jsonTestFile.json'
+        command='jq -r '.[${i}].keyboard' CloudInit_Options.json'
         keybrLay=($($command))
         echo "  Keyboard Layout:$keybrLay"
 
@@ -76,7 +77,7 @@ do
         # in arr2[i] command 2 for the ith vm will be stored       
         arr2+=("qm set $vmid --ciuser $ciuser --cipassword $cipassword --nameserver $nameserver --ipconfig0 gw=$gw,ip=$ip/24 --sshkey $sshkey --net0 model=$model,bridge=$bridge --keyboard $keybrLay")
 
-      	echo "======="
+	echo "======="
 
 done;
 
@@ -98,10 +99,10 @@ then
         done
 
 else
-        echo "Concel"
+  echo "Concel"
 fi
 
-#command=$(jq '.[1].vmid' jsonTestFile.json)
+#command=$(jq '.[1].vmid' CloudInit_Options.json)
 #echo $command
 
 
